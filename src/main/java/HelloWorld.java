9a0c5a68ba929006c9880bde0170c788c8449909
Andriy Shvets <andriy2.shvets@gmail.com>
2023-03-25 20:28:45 +0200

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld +++");
+        System.out.print("Hello\nagain");
+        System.out.print("\tand \"again\'");
+        System.out.print("\tand \"again\"");
+    }
+}
